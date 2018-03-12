dea1dba79037fe2d00530e2dacf700b951f46b2f
ThomasHaberl <t.haberl@gammadental.com>
2018-03-12 18:00:10 +0100

    Implemented tasks of lesson 3: Custom interface

---
+++ tasks/Task3/Task3/Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+
+namespace Task2
+{
+    interface IVehicle
+    {
+        string Manufacturer { get;  }
+        string Model { get; }
+
+        double Speed { get; }
+
+
+        void Accelerate();
+
+        void Decelerate();
+    }
+    
+    class Car
+        :IVehicle
+    {
+        private double _speed;
+
+
+
+        public Car(string manufacturer, string model)
+        {
+            if (string.IsNullOrEmpty(manufacturer))
+                throw new ArgumentException("String cannot be null or empty", nameof(manufacturer));
+
+            if (string.IsNullOrEmpty(model))
+                throw new ArgumentException("String cannot be null or empty", nameof(model));
+
+
+            Manufacturer = manufacturer;
+            Model = model;
+        }
+
+
+
+        public string Manufacturer { get; private set; }
+        public string Model { get; private set; }
+        
+        public double Speed
+        {
+            get { return _speed; }
+        }
+
+
+
+        public void Accelerate()
+        {
+            _speed += 1;
+        }
+
+        public void Decelerate()
+        {
+            if (_speed > 0)
+                _speed -= 1;
+        }
+    }
+
+    class Plane
+        : IVehicle
+    {
+        private double _speed;
+
+
+
+        public Plane(string manufacturer, string model)
+        {
+            if (string.IsNullOrEmpty(manufacturer))
+                throw new ArgumentException("String cannot be null or empty", nameof(manufacturer));
+
+            if (string.IsNullOrEmpty(model))
+                throw new ArgumentException("String cannot be null or empty", nameof(model));
+
+
+            Manufacturer = manufacturer;
+            Model = model;
+        }
+
+
+
+        public string Manufacturer { get; private set; }
+        public string Model { get; private set; }
+
+        public double Speed
+        {
+            get { return _speed; }
+        }
+
+
+
+        public void Accelerate()
+        {
+            _speed += 10;
+        }
+
+        public void Decelerate()
+        {
+            if (_speed > 0)
+                _speed -= 10;
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IVehicle[] vehicles = new IVehicle[]
+            {
+                new Car("Alpine", "A110"),
+                new Plane("Boeing", "747"),
+                new Car("Dacia", "Duster"),
+                new Plane("Airbus", "A380"),
+            };
+
+            foreach (IVehicle vehicle in vehicles)
+            {
+                Console.WriteLine($"{vehicle.Manufacturer} {vehicle.Model} - Current speed: {vehicle.Speed}");
+
+                vehicle.Accelerate();
+
+                Console.WriteLine($"{vehicle.Manufacturer} {vehicle.Model} - Current speed: {vehicle.Speed}");
+            }
+        }
+    }
+}
